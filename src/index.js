470d5bcd0c2af13633fb6438c4d6c777f863dfd4
KarlaMacedo <karlapaolamacedo@gmail.com>
2023-06-28 17:57:08 -0600

    configuración inicial, para comenzar a codear

---
+++ src/index.js
@@ -0,0 +1,11 @@
+// module.exports = () => {
+// eslint-disable-next-line linebreak-style
+// ...
+// }
+// eslint-disable-next-line import/no-extraneous-dependencies
+import chalk from 'chalk';
+
+// Ejemplo de uso de Chalk
+console.log(chalk.blue('Texto en azul'));
+console.log(chalk.green('Texto en verde'));
+console.log(chalk.red('Texto en rojo'));
