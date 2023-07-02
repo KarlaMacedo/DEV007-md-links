1d2ad63a615c546195ab872101fff69b53b1f0a5
KarlaMacedo <karlapaolamacedo@gmail.com>
2023-07-02 15:09:20 -0600

    solo lee algunos links con JSDOM

--- mdLinks.js
+++ mdLinks.js
@@ -1,10 +1,9 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import chalk from 'chalk';
 import fs from 'fs';
-// import { marked } from 'marked';
+import { marked } from 'marked';
 // import cheerio from 'cheerio';
-// import { JSDOM } from 'jsdom';
-import MarkdownIt from 'markdown-it';
+import { JSDOM } from 'jsdom';
 
 import getFiles from './getFiles.js';
 
@@ -18,38 +17,17 @@ console.log(chalk.bgRed(typeof filesArray));
 console.log(chalk.bgRed(filesArray));
 
 const mdlinks = (files) => {
-  const md = new MarkdownIt();
-
   files.forEach((content) => {
     const markdownContent = fs.readFileSync(content, 'utf-8');
-    const tokens = md.parse(markdownContent);
-    console.log(chalk.bgGray(tokens));
-    const links = [];
-    console.log(chalk.yellow(links));
-
-    tokens.forEach((token) => {
-      if (token.type === 'link_open') {
-        console.log(chalk.green(token.attrs));
-        const hrefAttr = token.attrs.find((attr) => attr[0] === 'href');
-        if (hrefAttr) {
-          links.push(hrefAttr[1]);
-        }
-      }
-    });
-
-    // MARKED Y JSDOM
-    /* const html = marked(markdownContent, { headerIds: false, mangle: false });
+    const html = marked(markdownContent, { headerIds: false, mangle: false });
     const dom = new JSDOM(html);
     const { document } = dom.window;
-    const links = Array.from(document.querySelectorAll('a')).map((link) => link.href); */
-
-    // MARKED Y CHEERIO
+    const links = Array.from(document.querySelectorAll('a')).map((link) => link.href);
     /* const $ = cheerio.load(html);
     const links = [];
     $('a').each((index, element) => {
       const link = $(element).attr('href');
       links.push(link); */
-
     console.log(`Enlaces encontrados en: ${content} 
       `);
     links.forEach((linked) => {
