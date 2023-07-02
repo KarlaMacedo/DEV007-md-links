5c5c249e8f838930eaf613cf668d5736e877f574
KarlaMacedo <karlapaolamacedo@gmail.com>
2023-07-02 15:17:36 -0600

    imprime los links con éxito, pero usa cheerio y dice que está en desuso

--- mdLinks.js
+++ mdLinks.js
@@ -2,8 +2,7 @@
 import chalk from 'chalk';
 import fs from 'fs';
 import { marked } from 'marked';
-// import cheerio from 'cheerio';
-import { JSDOM } from 'jsdom';
+import cheerio from 'cheerio';
 
 import getFiles from './getFiles.js';
 
@@ -20,24 +19,20 @@ const mdlinks = (files) => {
   files.forEach((content) => {
     const markdownContent = fs.readFileSync(content, 'utf-8');
     const html = marked(markdownContent, { headerIds: false, mangle: false });
-    const dom = new JSDOM(html);
-    const { document } = dom.window;
-    const links = Array.from(document.querySelectorAll('a')).map((link) => link.href);
-    /* const $ = cheerio.load(html);
+    const $ = cheerio.load(html);
     const links = [];
     $('a').each((index, element) => {
       const link = $(element).attr('href');
-      links.push(link); */
+      links.push(link);
+    });
     console.log(`Enlaces encontrados en: ${content} 
       `);
-    links.forEach((linked) => {
-      console.log(chalk.magenta(linked));
+    console.log(chalk.red(links));
+    links.forEach((linki) => {
+      console.log(chalk.magenta(linki));
     });
   });
 };
-
-// );
-// };
 
 mdlinks(filesArray);
 
