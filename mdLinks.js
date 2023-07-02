9ce5deb46b3a7073774e582848e9ae13bed141e3
KarlaMacedo <karlapaolamacedo@gmail.com>
2023-07-02 15:39:05 -0600

    leer links con async await y promesas

--- mdLinks.js
+++ mdLinks.js
@@ -15,23 +15,26 @@ getFiles(mainDirectory, filesArray);
 console.log(chalk.bgRed(typeof filesArray));
 console.log(chalk.bgRed(filesArray));
 
-const mdlinks = (files) => {
-  files.forEach((content) => {
-    const markdownContent = fs.readFileSync(content, 'utf-8');
-    const html = marked(markdownContent, { headerIds: false, mangle: false });
-    const $ = cheerio.load(html);
-    const links = [];
-    $('a').each((index, element) => {
-      const link = $(element).attr('href');
-      links.push(link);
-    });
-    console.log(`Enlaces encontrados en: ${content} 
-      `);
-    console.log(chalk.red(links));
-    links.forEach((linki) => {
-      console.log(chalk.magenta(linki));
-    });
-  });
+const mdlinks = async (files) => {
+  try {
+    await Promise.all(files.map(async (content) => {
+      const markdownContent = fs.readFileSync(content, 'utf-8');
+      const html = marked(markdownContent, { headerIds: false, mangle: false });
+      const $ = cheerio.load(html);
+      const links = [];
+      $('a').each((index, element) => {
+        const link = $(element).attr('href');
+        links.push(link);
+      });
+      console.log(`Enlaces encontrados en: ${content}`);
+      console.log(chalk.red(links));
+      await Promise.all(links.map(async (linki) => {
+        console.log(chalk.magenta(linki));
+      }));
+    }));
+  } catch (error) {
+    console.error('Error al obtener los archivos:', error);
+  }
 };
 
 mdlinks(filesArray);
