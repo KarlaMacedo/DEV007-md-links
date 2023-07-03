f21fabe017d93ca9142d042db0f86e45765cfb40
KarlaMacedo <karlapaolamacedo@gmail.com>
2023-07-03 14:47:48 -0600

    Union de funciones, mensajes de error, lectura path de archivos

--- getFiles.js
+++ getFiles.js
@@ -2,35 +2,50 @@ import chalk from 'chalk';
 import fs from 'fs';
 import path from 'path';
 
-// const mainDirectory = './tryOut';
+// const mainDirectory = './tryOut/file11.txt';
 // const filesArray = [];
 
 export default function getFiles(directory, array) {
-  try {
-    const files = fs.readdirSync(directory, 'utf8');
+  return new Promise((resolve, reject) => {
+    try {
+      const stats1 = fs.statSync(directory);
 
-    files.forEach((file) => {
-      const filePath = path.join(directory, file);
-      try {
-        const stats = fs.statSync(filePath);
-
-        if (stats.isFile() && path.extname(file) === '.md') {
-          array.push(filePath);
-          console.log(chalk.blue(filePath));
-        } else if (stats.isDirectory()) {
-          getFiles(filePath, array);
-        }
-      } catch (err) {
-        console.error('Error al obtener la estadística del archivo:', err);
+      if (stats1.isFile() && path.extname(directory) === '.md') {
+        array.push(directory);
+        resolve();
       }
-    });
-  } catch (err) {
-    console.error('Error al leer la carpeta:', err);
-  }
+      if (stats1.isFile() && path.extname(directory) !== '.md') {
+        reject(chalk.bgRedBright.bold(' ERROR: Tu archivo no es .md '));
+      }
+
+      const files = fs.readdirSync(directory, 'utf8');
+
+      files.forEach((file) => {
+        const filePath = path.join(directory, file);
+        try {
+          const stats = fs.statSync(filePath);
+
+          if (stats.isFile() && path.extname(file) === '.md') {
+            array.push(filePath);
+            // console.log(chalk.blue(filePath));
+          } else if (stats.isDirectory()) {
+            getFiles(filePath, array);
+          }
+        } catch (err) {
+          console.error(chalk.bgRedBright.bold(err));
+        }
+      });
+      resolve();
+    } catch (err) {
+      if (err.code === 'ENOENT') {
+        reject(chalk.bgRedBright.bold(' ERROR: Tu ruta no se encontró, revisa si es correcta '));
+      }
+    }
+  });
 }
 
 // Uso:
-/* console.log(chalk.bgGreen('TUS ARCHIVOS MD:'));
-getFiles(mainDirectory, filesArray);
-console.log(chalk.bgRed(typeof filesArray));
-console.log(chalk.bgRed(filesArray)); */
+// console.log(chalk.bgGreen('TUS ARCHIVOS MD:'));
+// getFiles(mainDirectory, filesArray);
+// console.log(chalk.bgBlue(typeof filesArray));
+// console.log(chalk.bgBlue(filesArray));
--- mdLinks.js
+++ mdLinks.js
@@ -10,26 +10,32 @@ import { JSDOM } from 'jsdom';
 import getFiles from './getFiles.js';
 
 // ------------ DIRECTORIO Y DONDE SE ALMACENARÁN FILES .md
-const mainDirectory = './tryOut';
+const mainDirectory = './tryOut/file11.md';
 const filesArray = [];
 
 // ----------- FUNCIÓN GET FILES .md
-console.log(chalk.bgGreen('TUS ARCHIVOS MD:'));
+/* console.log(chalk.bgGreen('TUS ARCHIVOS MD:'));
 getFiles(mainDirectory, filesArray);
 console.log(`
           `);
+console.log(chalk.bgRed(typeof getFiles(mainDirectory, filesArray)));
 console.log(chalk.bgRed(typeof filesArray));
 console.log(chalk.bgRed(filesArray));
 console.log(`
           `);
 
+mdlinks(filesArray); */
+
 // ------------ MDLINKS (BUSCAR LINKS DENTRO DE CADA ARCHIVO)
-const mdlinks = (files) => { // parámetro archivos .md
-  Promise.all( // trabaja con el array de promesas que se va a crear
+function mdlinks(files) { // parámetro archivos .md
+  if (files.length === 0) {
+    return console.error(chalk.bgRedBright.bold(' ERROR: No se encontró ningún archivo .md '));
+  }
+  return Promise.all( // trabaja con el array de promesas que se va a crear
     files.map((content) => new Promise((resolve, reject) => { // creación array de promesas de files
       fs.readFile(content, 'utf-8', (error, markdownContent) => { // lee cada archivo
         if (error) { // en caso de error
-          reject(error);
+          reject(chalk.bgRedBright.bold(error));
           return;
         }
 
@@ -47,28 +53,39 @@ const mdlinks = (files) => { // parámetro archivos .md
         const { document } = dom.window; // se ingresa al DOM
         const links = Array.from(document.querySelectorAll('a')).map((element) => element.href); // array de links
 
-        console.log(`Enlaces encontrados en: ${content}`);
-        console.log(chalk.red(links));
+        console.log(chalk.bold('Enlaces encontrados en: '), chalk.underline(content));
+        // console.log(chalk.red(links));
+
+        if (links.length === 0) {
+          console.log(chalk.bold.red('Este archivo no tiene links'));
+        }
 
         Promise.all( // trabaja con el array de promesas que se va a crear
           links.map((linki) => new Promise((resolved) => { // creación de array de promesas de links
-            resolved(console.log(chalk.magenta(linki)));
+            resolved(console.log(chalk.blue(linki)));
             // ejecutar como resuelta promesas de links cuando termine
           })),
         ).then(() => {
-          resolve(console.log(`
-          `)); // ejecutar como resuelta promesas de files cuando termine
+          resolve(console.log('')); // ejecutar como resuelta promesas de files cuando termine
         }).catch((errors) => {
-          reject(errors); // en caso de error en promesas de files, ejecutar error
+          reject(chalk.bgRedBright.bold(errors)); // error en promesas de files, ejecutar error
         });
       });
     })),
   ).then(() => {
     console.log('Proceso finalizado'); // saber que finalizó la función
   });
-};
+}
 
-mdlinks(filesArray);
+function executeFunctions() {
+  (getFiles(mainDirectory, filesArray)).then(
+    () => mdlinks(filesArray), // Si la función 1 tuvo éxito, entonces ejecuta la función 2
+  ).catch((error) => {
+    console.error(chalk.bgRedBright.bold(error));
+  });
+}
+
+executeFunctions();
 
 /* mdlinks.then((result) => {
   console.log(result);
