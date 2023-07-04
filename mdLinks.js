796a37f8a012ad9c8bc0fc93d0f8c7224902b159
KarlaMacedo <karlapaolamacedo@gmail.com>
2023-07-03 21:03:27 -0600

    valid false, no valid e inicios de true

--- mdLinks.js
+++ mdLinks.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 // ----------- IMPORTAR LIBRERÍAS
 /* eslint-disable import/no-extraneous-dependencies */
 import chalk from 'chalk';
@@ -10,7 +11,7 @@ import { JSDOM } from 'jsdom';
 import getFiles from './getFiles.js';
 
 // ------------ DIRECTORIO Y DONDE SE ALMACENARÁN FILES .md
-const mainDirectory = './tryOut/file11.md';
+const mainDirectory = './tryOut';
 const filesArray = [];
 
 // ----------- FUNCIÓN GET FILES .md
@@ -27,12 +28,12 @@ console.log(`
 mdlinks(filesArray); */
 
 // ------------ MDLINKS (BUSCAR LINKS DENTRO DE CADA ARCHIVO)
-function mdlinks(files) { // parámetro archivos .md
-  if (files.length === 0) {
+function mdlinks(path, options) { // parámetro archivos .md
+  if (path.length === 0) {
     return console.error(chalk.bgRedBright.bold(' ERROR: No se encontró ningún archivo .md '));
   }
   return Promise.all( // trabaja con el array de promesas que se va a crear
-    files.map((content) => new Promise((resolve, reject) => { // creación array de promesas de files
+    path.map((content) => new Promise((resolve, reject) => { // creación array de promesas de files
       fs.readFile(content, 'utf-8', (error, markdownContent) => { // lee cada archivo
         if (error) { // en caso de error
           reject(chalk.bgRedBright.bold(error));
@@ -53,23 +54,72 @@ function mdlinks(files) { // parámetro archivos .md
         const { document } = dom.window; // se ingresa al DOM
         const links = Array.from(document.querySelectorAll('a')).map((element) => element.href); // array de links
 
-        console.log(chalk.bold('Enlaces encontrados en: '), chalk.underline(content));
-        // console.log(chalk.red(links));
+        if (options === false) {
+          const option = {
+            validate: options,
+          };
+          const linkos = Array.from(document.querySelectorAll('a')).map((element) => ({
+            href: element.href,
+            text: element.textContent.trim(),
+            file: content,
+          }));
+          // console.log(chalk.magenta(option.validate));
+          // console.log(chalk.magenta(linkos));
+          console.log('');
+          console.log(chalk.bold('Links encontrados en: '), chalk.underline(content));
+          linkos.forEach((element) => {
+            console.log('href: ', chalk.magenta(element.href));
+            console.log('text: ', chalk.magenta(element.text));
+            console.log('');
+          });
+        }
 
         if (links.length === 0) {
           console.log(chalk.bold.red('Este archivo no tiene links'));
         }
 
-        Promise.all( // trabaja con el array de promesas que se va a crear
+        if (!options && options !== false) {
+          const option = {
+            validate: undefined,
+          };
+          // console.log(chalk.magenta(option.validate));
+          reject(new Error(chalk.bgRedBright.bold('No elegiste ninguna opción ')));
+          return;
+        }
+
+        if (options === true) {
+          const option = {
+            validate: options,
+          };
+          console.log(chalk.magenta(option.validate));
+          const linkos = Array.from(document.querySelectorAll('a')).map((element) => ({
+            href: element.href,
+            text: element.textContent.trim(),
+            file: content,
+          }));
+          console.log('');
+          console.log(chalk.bold('Links encontrados en: '), chalk.underline(content));
+          linkos.forEach((element) => {
+            console.log('href: ', chalk.blue(element.href));
+            console.log('text: ', chalk.blue(element.text));
+            console.log('');
+          });
+        }
+
+        // console.log(chalk.bold('Links encontrados en: '), chalk.underline(content));
+        // console.log(chalk.red(links));
+
+        /* Promise.all( // trabaja con el array de promesas que se va a crear
           links.map((linki) => new Promise((resolved) => { // creación de array de promesas de links
             resolved(console.log(chalk.blue(linki)));
             // ejecutar como resuelta promesas de links cuando termine
           })),
-        ).then(() => {
-          resolve(console.log('')); // ejecutar como resuelta promesas de files cuando termine
-        }).catch((errors) => {
-          reject(chalk.bgRedBright.bold(errors)); // error en promesas de files, ejecutar error
-        });
+        ) */
+        /* .then(() => {
+            resolve(console.log('')); // ejecutar como resuelta promesas de files cuando termine
+          }).catch((errors) => {
+            reject(chalk.bgRedBright.bold(errors)); // error en promesas de files, ejecutar error
+          }); */
       });
     })),
   ).then(() => {
@@ -79,7 +129,7 @@ function mdlinks(files) { // parámetro archivos .md
 
 function executeFunctions() {
   (getFiles(mainDirectory, filesArray)).then(
-    () => mdlinks(filesArray), // Si la función 1 tuvo éxito, entonces ejecuta la función 2
+    () => mdlinks(filesArray, true), // Si la función 1 tuvo éxito, entonces ejecuta la función 2
   ).catch((error) => {
     console.error(chalk.bgRedBright.bold(error));
   });
